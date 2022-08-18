193e359f8c5570f5a9e7dc094af6de2254e6c091
marstamm <martin.stamm@camunda.com>
2022-08-18 13:27:41 +0000

    Build: (73b4705) chore: update moderators

--- release-issue/index.js
+++ release-issue/index.js
@@ -13338,9 +13338,13 @@ try {
 /***/ ((module) => {
 
 module.exports = [
-  { idx: 0, fullName: 'Martin Stamm', login: 'marstamm' },
-  { idx: 1, fullName: 'Martin Stamm', login: 'marstamm' },
-  { idx: 2, fullName: 'Martin Stamm', login: 'marstamm' },
+  { idx: 0, fullName: 'Maciej Barelkowski', login: 'barmac' },
+  { idx: 1, fullName: 'Philipp Fromme', login: 'philippfromme' },
+  { idx: 2, fullName: 'Niklas Kiefer', login: 'pinussilvestrus' },
+  { idx: 3, fullName: 'Nico Rehwaldt', login: 'nikku' },
+  { idx: 4, fullName: 'Martin Stamm', login: 'marstamm' },
+  { idx: 5, fullName: 'Beatriz Mendes', login: 'smbea' },
+  { idx: 6, fullName: 'Valentin Serra', login: 'Skaiir' }
 ];
 
 
