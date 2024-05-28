ca91a84442dcb73441591ef47b6fc5cc81a98e76
Vinicius Goulart <vinicius.goulart@camunda.com>
2024-05-28 17:20:50 +0200

    chore: fix quotes

--- src/weekly-notes/index.js
+++ src/weekly-notes/index.js
@@ -131,7 +131,7 @@ Assigned ${nextRoleMessage}.`
       weeklyNote = weeklyNote.replaceAll(`{{${role}}}`, `@${login}`);
     });
 
-    weeklyNote = weeklyNote.replaceAll(`{{previousIssueURL}}`, `${previousIssueURL}`);
+    weeklyNote = weeklyNote.replaceAll('{{previousIssueURL}}', `${previousIssueURL}`);
     weeklyNote = withoutPrelude(weeklyNote);
 
     return weeklyNote;
