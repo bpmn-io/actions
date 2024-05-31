89ae3c1e8c449727ff1fafcbe774699e80b0dcec
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2024-05-31 09:33:52 +0200

    chore: add shared `getNextAssignee` utility

---
+++ src/shared/test.js
@@ -0,0 +1,24 @@
+const {
+  getNextAssignee
+} = require('./util.js');
+
+const assert = require('node:assert');
+
+const assignees = [
+  { login: 'walt', fullName: 'WALT' },
+  { login: 'lisa', fullName: 'LISA' },
+  { login: 'bert', fullName: 'Bert' }
+];
+
+function verify(currentLogin, nextLogin, offset) {
+
+  const next = getNextAssignee(assignees, { login: currentLogin }, offset);
+
+  assert.equal(next?.login, nextLogin);
+}
+
+verify('walt', 'lisa');
+verify('unknown', null);
+verify('bert', 'walt');
+verify('walt', 'bert', 2);
+verify('walt', 'walt', 3);
