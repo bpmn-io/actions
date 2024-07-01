647f5df0beef224a74cef383bb17b6a0816e2486
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2024-07-01 17:05:14 +0200

    chore: update DEFAULT_MODERATORS

--- src/shared/DEFAULT_MODERATORS.js
+++ src/shared/DEFAULT_MODERATORS.js
@@ -2,5 +2,6 @@ module.exports = [
   { idx: 0, fullName: 'Maciej Barelkowski', login: 'barmac' },
   { idx: 1, fullName: 'Philipp Fromme', login: 'philippfromme' },
   { idx: 2, fullName: 'Nico Rehwaldt', login: 'nikku' },
-  { idx: 3, fullName: 'Martin Stamm', login: 'marstamm' }
+  { idx: 3, fullName: 'Martin Stamm', login: 'marstamm' },
+  { idx: 4, fullName: 'Jarek Danielak', login: 'jarekdanielak' }
 ];
