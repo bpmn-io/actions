e8c4fc05154a3bf2dd18435a29388ffe529c1c9b
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2024-09-10 15:48:44 +0200

    fix: properly handle no last assignee

--- src/shared/util.js
+++ src/shared/util.js
@@ -6,19 +6,16 @@ const { findIndex } = require('min-dash');
 
 /**
  * @param { Assignee[] } candidates
- * @param { { login: string } } lastAssignee
+ * @param { { login: string } } [lastAssignee]
  * @param {number} [offset=1]
  *
  * @return {Assignee | null}
  */
 function getNextAssignee(candidates, lastAssignee, offset = 1) {
-  const lastIndex = findIndex(candidates, c => c.login === lastAssignee.login);
+  const lastIndex = findIndex(candidates, c => c.login === lastAssignee?.login);
 
-  // ensure assignee was a valid moderator
-  if (lastIndex === -1) {
-    return null;
-  }
-
+  // if last assignee was not found, then we default to
+  // first candidate
   return candidates[(lastIndex + offset) % candidates.length];
 }
 
