66f353092690fcb017182081e2357b97e3952cee
barmac <maciejbarel@gmail.com>
2025-01-16 15:25:09 +0000

    Build: (1884e6f) chore: remove Nico from default moderators list

--- release-issue/index.js
+++ release-issue/index.js
@@ -32449,7 +32449,6 @@ function wrappy (fn, cb) {
 module.exports = [
   { idx: 0, fullName: 'Maciej Barelkowski', login: 'barmac' },
   { idx: 1, fullName: 'Philipp Fromme', login: 'philippfromme' },
-  { idx: 2, fullName: 'Nico Rehwaldt', login: 'nikku' },
   { idx: 3, fullName: 'Michal Konopski', login: 'misiekhardcore' },
   { idx: 4, fullName: 'Jarek Danielak', login: 'jarekdanielak' },
   { idx: 5, fullName: 'Abdul Ahad', login: 'abdul99ahad' }
