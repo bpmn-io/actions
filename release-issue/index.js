be8187530613bc5f19f37d5ccd32bea72e03472b
barmac <maciejbarel@gmail.com>
2025-02-04 08:37:56 +0000

    Build: (d6e8a29) chore: update default moderators list
    Related to https://github.com/bpmn-io/internal-docs/issues/1123

--- release-issue/index.js
+++ release-issue/index.js
@@ -32449,9 +32449,8 @@ function wrappy (fn, cb) {
 module.exports = [
   { idx: 0, fullName: 'Maciej Barelkowski', login: 'barmac' },
   { idx: 1, fullName: 'Philipp Fromme', login: 'philippfromme' },
-  { idx: 3, fullName: 'Michal Konopski', login: 'misiekhardcore' },
-  { idx: 4, fullName: 'Jarek Danielak', login: 'jarekdanielak' },
-  { idx: 5, fullName: 'Abdul Ahad', login: 'abdul99ahad' }
+  { idx: 2, fullName: 'Jarek Danielak', login: 'jarekdanielak' },
+  { idx: 3, fullName: 'Abdul Ahad', login: 'abdul99ahad' }
 ];
 
 
