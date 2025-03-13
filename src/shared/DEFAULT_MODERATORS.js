62618b34274cf8210bc210697625cc1843cb931a
Simon <Buckwich@users.noreply.github.com>
2025-03-13 10:22:29 +0100

    chore: add Simon to DEFAULT_MODERATORS.js

--- src/shared/DEFAULT_MODERATORS.js
+++ src/shared/DEFAULT_MODERATORS.js
@@ -2,5 +2,7 @@ module.exports = [
   { idx: 0, fullName: 'Maciej Barelkowski', login: 'barmac' },
   { idx: 1, fullName: 'Philipp Fromme', login: 'philippfromme' },
   { idx: 2, fullName: 'Jarek Danielak', login: 'jarekdanielak' },
-  { idx: 3, fullName: 'Abdul Ahad', login: 'abdul99ahad' }
+  { idx: 3, fullName: 'Abdul Ahad', login: 'abdul99ahad' },
+  { idx: 4, fullName: 'Simon Steinrücken', login: 'Buckwich' }
+  
 ];
