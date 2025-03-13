7b6a5324b15c7fe342057da14e4b1aed57821625
Buckwich <Buckwich@users.noreply.github.com>
2025-03-13 09:22:52 +0000

    Build: (62618b3) chore: add Simon to DEFAULT_MODERATORS.js

--- weekly-notes/index.js
+++ weekly-notes/index.js
@@ -29901,7 +29901,9 @@ module.exports = [
   { idx: 0, fullName: 'Maciej Barelkowski', login: 'barmac' },
   { idx: 1, fullName: 'Philipp Fromme', login: 'philippfromme' },
   { idx: 2, fullName: 'Jarek Danielak', login: 'jarekdanielak' },
-  { idx: 3, fullName: 'Abdul Ahad', login: 'abdul99ahad' }
+  { idx: 3, fullName: 'Abdul Ahad', login: 'abdul99ahad' },
+  { idx: 4, fullName: 'Simon Steinrücken', login: 'Buckwich' }
+  
 ];
 
 
