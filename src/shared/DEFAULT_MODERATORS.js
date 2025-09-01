c45ace6961bd4dc0f426f82a947728c143298c35
Maciej Barelkowski <28307541+barmac@users.noreply.github.com>
2025-09-01 10:54:24 +0200

    chore: update moderators list

--- src/shared/DEFAULT_MODERATORS.js
+++ src/shared/DEFAULT_MODERATORS.js
@@ -2,7 +2,5 @@ module.exports = [
   { idx: 0, fullName: 'Maciej Barelkowski', login: 'barmac' },
   { idx: 1, fullName: 'Philipp Fromme', login: 'philippfromme' },
   { idx: 2, fullName: 'Jarek Danielak', login: 'jarekdanielak' },
-  { idx: 3, fullName: 'Abdul Ahad', login: 'abdul99ahad' },
-  { idx: 4, fullName: 'Simon Steinrücken', login: 'Buckwich' }
-  
+  { idx: 3, fullName: 'Simon Steinrücken', login: 'Buckwich' }
 ];
