eeafba662544dd573f2a30e248cbefa4737e3110
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2025-10-08 09:37:55 +0200

    chore: use mocha & chai for tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,5 +10,9 @@ export default [
   {
     ignores: files.ignored
   },
-  ...bpmnIoPlugin.configs.node
+  ...bpmnIoPlugin.configs.node,
+  ...bpmnIoPlugin.configs.mocha.map(config => ({
+    files: [ '**/*test.js', '**/test/**/*.js' ],
+    ...config
+  }))
 ];
